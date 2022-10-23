7fe77f2faa070112522a897e316df2a4ebb75b5b
fer0o <fer0osennin@gmail.com>
2022-10-23 16:29:26 -0500

    experience and experience card done, miss change values with props

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -12,7 +12,7 @@ export default function Experience ({}: Props) {
       transition={{ duration: 1.5 }}
       className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
     >
-      <h3 className='absolute top-32 uppercase tracking-[20px] text-white text-2xl'>
+      <h3 className='absolute top-28 uppercase tracking-[20px] text-white text-2xl'>
         Experience
       </h3>
       <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
