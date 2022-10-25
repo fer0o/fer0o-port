e16bcf5af531035b8fe1b756fa822f4ca1b23ccf
fer0o <fer0osennin@gmail.com>
2022-10-25 13:19:45 -0500

    minor changes index

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import Skills from '../components/Skills'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-black text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+    <div className='bg-black text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0'>
       <Head>
         <title>Fer's Portfolios</title>
       </Head>
